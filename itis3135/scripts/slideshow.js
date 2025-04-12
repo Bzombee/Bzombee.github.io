2b3646e41a4e6de1aaca549c3a941c70754f13e9
Brian Zielinski <bzielins@uncc.edu>
2025-04-12 17:47:33 -0400

    slideshow changes

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -3,7 +3,7 @@ let slideIndex = 1;
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("demo");
+    let thumbnail = document.getElementsByClassName("thumbnail-demo");
     let captionText = document.getElementById("caption");
     if (n > slides.length) {
         slideIndex = 1;
@@ -14,14 +14,14 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < thumbnail.length; i++) {
+      thumbnail[i].className = thumbnail[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+    thumbnail[slideIndex-1].className += " active";
+    captionText.innerHTML = thumbnail[slideIndex-1].alt;
 
-    dots[slideIndex - 1].scrollIntoView({
+    thumbnail[slideIndex - 1].scrollIntoView({
         behavior: "smooth",
         inline: "center", // center horizontally
         block: "nearest"  // avoid vertical scroll
